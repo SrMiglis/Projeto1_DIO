09f2f0aa6c7b6edb794dfd26744f4a3c72094735
SrMiglis <miguelmoreiraalves222@gmail.com>
2025-03-29 22:54:58 -0300

    Segunda mudança
    Fiz o verso frente e trás no rg
    
    --SrMiglis

--- Creeper.lua
+++ Creeper.lua
@@ -8,9 +8,10 @@ os.execute("chcp 65001")
 
 -- Criatura
 local NomeMonstro = "Creeper"
-local Descricao = "Tome cuidado quando for minerar, você pode ouvir um TSSSSSSSS"
-local emoji = "💥"
-
+local Descricao = "Um monstro furtivo que espere o exato momento quando voce esta distraido para atacar"
+local Emoji = "💣"
+local Som = "TSSS BOOM"
+local Hour = "Noite"
 -- Atributos
 local Vida = 10
 local Ataque = 8
@@ -37,25 +38,82 @@ local function BarradeProgresso(atributo)
     return result 
 end
 -- Cartão
-print("------------------------------------------------")
-print("| "..NomeMonstro)
-print("| "..Descricao)
-print("| "..emoji)
-print("| FOTO DO MELIANTE")
-print("|🟩🟩🟩🟩🟩🟩🟩🟩")
-print("|🟩⬛⬛🟩🟩⬛⬛🟩")
-print("|🟩⬛⬛🟩🟩⬛⬛🟩")
-print("|🟩🟩🟩⬛⬛🟩🟩🟩")
-print("|🟩🟩🟩⬛⬛🟩🟩🟩")
-print("|🟩🟩⬛⬛⬛⬛🟩🟩")
-print("|🟩🟩⬛🟩🟩⬛🟩🟩")
-print("|🟩🟩🟩🟩🟩🟩🟩🟩")
-print("|")
-print("| Atributos")
-print("|")
-print("| Vida =         ".. BarradeProgresso(Vida))
-print("| Defesa =       ".. BarradeProgresso(Defesa))
-print("| Ataque =       ".. BarradeProgresso(Ataque))
-print("| Inteligencia = ".. BarradeProgresso(Inteligencia))
-print("| Velocidade =   ".. BarradeProgresso(Velocidade))
-print("------------------------------------------------")
+-- Implementei está função que vai representar o verso da frente do RG
+local function ExibirFrente()
+    print ("▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃")
+    print(" ▮")
+    print(" ▮" ..   NomeMonstro)
+    print(" ▮  Tipo monstro explosivo"..Emoji)
+    print(" ▮    Sua foto:")
+    print(" ▮")
+    print(" ▮⠀ ⠀⠀⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿")
+    print(" ▮⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿")
+    print(" ▮⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿")
+    print(" ▮⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿")
+    print(" ▮⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⢸⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+    print(" ▮⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀")
+    print(" ▮⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀")
+    print(" ▮⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀")
+    print(" ▮⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀")
+    print(" ▮⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀")
+    print(" ▮⠀⠀⠀⠀⠀ ⢸⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ ⢸⣿⣿⣿⡇⠀⠀⠀⠀⠀")
+    print(" ▮⠀⠀⠀⠀⠀ ⢸⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ ⢸⣿⣿⣿⡇     ")
+    print(" ▮")
+    print(" ▮")
+    print(" ▮")
+    print(" ▮")
+     print ("▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃")
+end
+
+--Verso do cartão
+--Função que vai exibir o verso de trás do RG
+local function ExibirVerso()
+    
+
+
+    print ("▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃")
+    print(" ▮")
+    print(" ▮   Informações sobre Creepper")
+    print(" ▮" .. Descricao..Emoji)
+    print(" ▮")
+    print(" ▮   Onde o mostro nasce: Qualquer lugar exceto o Oceano e a dimensão do End")
+    print(" ▮")
+    print(" ▮   Comportamento: Hosti😈")
+    print(" ▮")
+    print(" ▮  Sua fraqueza: Gatos😼")
+    print(" ▮")
+    print(" ▮  Seu som: "..Som)
+    print(" ▮")
+    print ("▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃▃")
+
+end
+-- Variável de controle do lado do RG
+local ladoRG = "frente"
+
+-- Loop para permitir alternar entre frente e verso
+while true do
+    os.execute("cls") -- Limpa o terminal (Windows)
+    
+    if ladoRG == "frente" then
+        ExibirFrente()
+    else
+        ExibirVerso()
+    end
+
+    -- Pergunta ao usuário
+    print("\nDigite [1] para virar o RG ou [0] para sair:")
+    local opcao = io.read()
+
+    if opcao == "1" then
+        -- Alternar entre frente e verso
+        if ladoRG == "frente" then
+            ladoRG = "verso"
+        else
+            ladoRG = "frente"
+        end
+    elseif opcao == "0" then
+        break -- Sai do loop
+    else
+        print("Opção inválida! Tente novamente.")
+    end
+end
